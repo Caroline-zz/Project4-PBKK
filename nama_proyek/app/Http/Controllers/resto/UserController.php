17a67e340d1d2a7cdc9dc9d0663200b2fbed097c
caroline-zz <caroline.haeca@gmail.com>
2023-07-02 15:53:31 +0700

    pr7

--- nama_proyek/app/Http/Controllers/resto/UserController.php
+++ nama_proyek/app/Http/Controllers/resto/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\resto;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
--- nama_proyek/resources/views/layouts/app.blade.php
+++ nama_proyek/resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/pizza.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
